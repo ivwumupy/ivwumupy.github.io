4797dcb8a06fcf3e4c417bc5e702c51c1db892f9
Tianjiao Nie <ivwumupy@gmail.com>
2023-12-01 21:26:27 +0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, ... }@attrs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system} = {
+        default = pkgs.mkShellNoCC {
+          nativeBuildInputs = with pkgs; [
+            nodejs
+          ];
+        };
+      };
+    };
+}
